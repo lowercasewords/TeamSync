24c1b5f2518380a5335a53265ce1e0129db845a0
Your Name <your@email.com>
2025-11-28 14:59:47 -0800

    implementing Login class

---
+++ login.py
@@ -0,0 +1,15 @@
+from flask import render_template, request, url_for, redirect, session
+from flask.views import MethodView
+from entrymodel import get_model as get_user_model
+
+class Login(MethodView):
+    def get(self):
+        session.clear()
+        return render_template("login.html")
+    def post(self):
+        users_model = get_user_model()
+        user = users_model.get_user_by_email(request.form['email'])
+        if type(user) is None:
+            return self.get()
+        session["user"] = user
+        return redirect(url_for('dashboard'))
