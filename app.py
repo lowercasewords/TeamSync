fc0e21e9a7d8057024c83eafbee3869ad42a06e1
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2025-11-28 13:11:24 -0800

    base front-end

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# Debugging 
+@app.route("/login", methods=["GET", 'POST'])
+def login():
+    return render_template("login.html")
+
+# Debugging 
+@app.route("/logout", methods=['POST'])
+def logout():
+    return render_template("login.html")
+
+# Debugging 
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+# Debugging 
+@app.route('/create_event', methods=['GET'])
+def create_event():
+    return render_template('create_event.html')
+
+# Debugging 
+@app.route('/new_user', methods=['GET'])
+def new_user():
+    return render_template('new_user.html')
 
 if __name__ == '__main__':
     app.run(debug=True) # debug=True enables reloader and debugger for development
