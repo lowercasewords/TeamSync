0ed168ba840f55183ccfdaa04f7786b387eb0760
Your Name <your@email.com>
2025-11-28 15:08:46 -0800

    implementing Dashboard class

---
+++ dashboard.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from flask.views import MethodView
+from entrymodel import get_model as get_user_model
+from eventsmodel import get_model as get_event_model
+
+class Dashboard(MethodView):
+    def get(self):
+        users_model = get_user_model();
+        events_model = get_event_model();
+        users = users_model.get_all_users();
+        events = events_model.get_all_events();
+        return render_template('index.html', users=users, events=events)
