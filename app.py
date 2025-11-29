d43c22cfd7a47bdfa81ef3c513697043de4472d5
Your Name <your@email.com>
2025-11-28 16:32:03 -0800

    adding /logout route, fixing url_for routings

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask.views import MethodView
 from login import Login
+from logout import Logout
 from dashboard import Dashboard
 from event import Event
 from user import User
@@ -14,16 +15,20 @@ app.add_url_rule('/',
                  view_func=Login.as_view('login'),
                  methods=["GET", "POST"])
 
+app.add_url_rule('/logout',
+                 view_func=Logout.as_view('logout'),
+                 methods=["POST"])
+
 app.add_url_rule('/dashboard',
                  view_func=Dashboard.as_view('dashboard'),
                  methods=["GET"])
 
-app.add_url_rule('/event',
-                 view_func=Event.as_view('event'),
+app.add_url_rule('/create_event',
+                 view_func=Event.as_view('create_event'),
                  methods=["GET"])
 
-app.add_url_rule('/user',
-                 view_func=User.as_view('user'),
+app.add_url_rule('/new_user',
+                 view_func=User.as_view('new_user'),
                  methods=["GET"])
 
 if __name__ == '__main__':
