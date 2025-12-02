e8819405a83a3799abbf9e3cfe9696c88998b392
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2025-12-01 23:25:49 -0800

    server denies invalid role actions

--- event.py
+++ event.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, abort
 from flask.views import MethodView
 
 class Event(MethodView):
@@ -6,5 +6,7 @@ class Event(MethodView):
         user = session.get("user")
         if user is None:
             return redirect(url_for('login'))
+        elif user["role"] == "read":
+            abort(403, "Invalid permissions")
         else:
             return render_template('create_event.html', user=user)
